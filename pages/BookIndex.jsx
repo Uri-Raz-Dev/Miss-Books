688b5597abbee309eb5f4925ca92805f94b62ac7
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-05-04 15:53:59 +0300

    Create book service with async stoarge

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -1,11 +1,8 @@
 import { bookService } from "../services/book.service.js"
 
 const { useState, useEffect, useRef } = React
-bookService
 
 export function BookIndex() {
-    useEffect(() => {
-        console.log(bookService._createBooks());
-    }, []);
+
     return <h1>book</h1>
 }
--- services/book.service.js
+++ services/book.service.js
@@ -6,15 +6,74 @@ const BOOK_KEY = 'bookDB'
 _createBooks()
 
 export const bookService = {
-    _createBooks
+    query,
+    get,
+    remove,
+    save
+}
+window.bs = bookService
+
+function query(filterBy = {}) {
+    return storageService.query(BOOK_KEY)
+        .then(books => {
+            if (filterBy.title) {
+                const regExp = new RegExp(filterBy.title, 'i')
+
+                books = books.filter(book => regExp.test(book.title))
+
+            }
+
+            if (filterBy.price) {
+                books = books.filter((book) => {
+                    const { listPrice: { amount } } = book
+                    const bookPrice = amount
+                    return bookPrice
+                })
+
+            }
+            return books
+        })
 }
 
 
-function _createBook(title, price, isOnSale, currencyCode) {
+function get(bookId) {
+    return storageService.get(BOOK_KEY, bookId)
+        .then(book => {
+            book = _setNextPrevCarId(book)
+            return book
+        })
+}
+
+function remove(bookId) {
+    return storageService.remove(BOOK_KEY, bookId)
+}
+
+function save(book) {
+    if (book.id) {
+        return storageService.put(BOOK_KEY, book)
+    } else {
+        return storageService.post(BOOK_KEY, book)
+    }
+}
+
+function _setNextPrevCarId(book) {
+    return storageService.query(BOOK_KEY).then((books) => {
+        const bookIdx = books.findIndex((currBook) => currBook.id === book.id)
+        const nextbook = books[bookIdx + 1] ? books[bookIdx + 1] : books[0]
+        const prevbook = books[bookIdx - 1] ? books[bookIdx - 1] : books[books.length - 1]
+        book.nextbookId = nextbook.id
+        book.prevbookId = prevbook.id
+        return book
+    })
+}
+
+function _createBook(title, price, isOnSale, currencyCode, idx) {
 
     const book = {
         id: utilService.makeId(),
         title,
+        description: utilService.makeLorem(10),
+        thumbnail: `BooksImages/${idx}.jpg`,
         listPrice: {
             amount: currencyCode === 'USD' ? price + '$' : price + '€',
             currencyCode,
@@ -30,9 +89,9 @@ function _createBooks() {
         bookList = []
 
 
-        bookList.push(_createBook('Gwent', 50, true, 'EUR'),
-            _createBook('Between Here And Gone', 30, false, 'USD'),
-            _createBook('Magic Lantern', 25, true, 'EUR'))
+        bookList.push(_createBook('Gwent', 50, true, 'EUR', 1),
+            _createBook('Between Here And Gone', 30, false, 'USD', 2),
+            _createBook('Magic Lantern', 25, true, 'EUR', 3))
 
 
         utilService.saveToStorage(BOOK_KEY, bookList)
