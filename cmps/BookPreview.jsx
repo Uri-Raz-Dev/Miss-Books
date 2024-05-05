2ded2ab1d4605c64ccc287f9ba4607af7b580987
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-05-05 20:38:06 +0300

    Refactor BookPreview

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,11 +1,12 @@
 
 export function BookPreview({ book }) {
-    const { title, description, listPrice: { amount, isOnSale }, thumbnail } = book
+    const { title, subtitle, listPrice: { amount, isOnSale }, thumbnail, authors, publishedDate } = book
     return <section className="book-preview grid">
         <img src={thumbnail} alt="book tumbnail" />
 
-        <h2>{title}</h2>
-        <p>{description}</p>
+        <h2>{`${title} (${subtitle})`}</h2>
+        <h3>{`by ${[authors]}`}</h3>
+        <p>{`Published ${publishedDate}`}</p>
         <span className="price">{amount}</span>
         {isOnSale && <span className="sale">On Sale!</span>}
 
