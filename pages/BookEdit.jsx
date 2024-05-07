5fcb5620b817d431167bbff21241d74c41105172
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-05-07 22:27:20 +0300

    Create BookEdit

--- pages/BookEdit.jsx
+++ pages/BookEdit.jsx
@@ -28,26 +28,41 @@ export function BookEdit() {
     }
 
     function handleChange({ target }) {
-        const { type, name: prop } = target
-        let { value } = target
+        const { type, name: prop } = target;
+        let { value } = target;
 
         switch (type) {
             case 'range':
             case 'number':
-                value = +value
+                value = +value;
                 break;
 
             case 'checkbox':
-                value = target.checked
+                value = target.checked;
                 break;
-        }
-        setBook(prevBook => ({ ...prevBook, [prop]: value }))
 
+            case 'file':
+                const file = target.files[0]
+                if (file) {
+                    const reader = new FileReader()
+                    reader.onload = (e) => {
+                        const imagePath = e.target.result
+                        setBook(prevState => ({
+                            ...prevState,
+                            thumbnail: imagePath
+                        }))
+                    }
+                    reader.readAsDataURL(file)
+                }
+                return
+        }
+
+        setBook(prevBook => ({ ...prevBook, [prop]: value }));
     }
 
     let { title, subtitle, description, listPrice: { amount, isOnSale }, thumbnail, authors, categories
         , publishedDate, pageCount, language } = book
-
+    console.log(description);
     return <section className="book-edit">
         <h1>{params.bookId ? 'Edit book' : 'Add book'}</h1>
 
@@ -57,12 +72,61 @@ export function BookEdit() {
                 onChange={handleChange} value={title}
                 id="title" name="title"
                 type="text" placeholder="title" />
+            <label htmlFor="subtitle">Book Sub-Title</label>
+            <input
+                onChange={handleChange} value={subtitle}
+                id="subtitle" name="subtitle"
+                type="text" placeholder="sub-title" />
 
-            <label htmlFor="price">Book Price</label>
+            <label htmlFor="author">Authors</label>
+            <input
+                onChange={handleChange} value={[authors]}
+                id="author" name="authors"
+                type="text" placeholder="authors" />
+
+            <label htmlFor="category">Categories</label>
+            <input
+                onChange={handleChange} value={categories}
+                id="category" name="category"
+                type="text" placeholder="categories" />
+
+            <label htmlFor="description">Description</label>
+            <textarea onChange={handleChange} value={description}
+                id="description" name="description"
+                type="text" rows={10} cols={40} placeholder="description" >
+
+            </textarea>
+
+            <label htmlFor="page-count">Page Count</label>
+            <input
+                onChange={handleChange} value={pageCount}
+                id="page-count" name="page-count"
+                type="number" placeholder="Page count" />
+            <label htmlFor="amount">Price</label>
             <input
                 onChange={handleChange} value={parseFloat(amount)}
                 id="amount" name="amount"
-                type="number" placeholder="price" />
+                type="number" placeholder="Price" />
+
+            <label htmlFor="published-date">Publish date</label>
+            <input
+                onChange={handleChange} value={publishedDate}
+                id="published-date" name="published-date"
+                type="number" placeholder="Publish Date" />
+
+            <label htmlFor="language">Book Language</label>
+            <input
+                onChange={handleChange} value={language}
+                id="language" name="language"
+                type="text" placeholder="language" />
+
+            <label htmlFor="thumbnail">Choose Thumbnail</label>
+            <input
+
+                onChange={handleChange}
+                accept="image/png, image/jpeg"
+                id="thumbnail" name="thumbnail"
+                type="file" />
 
             <button>Save</button>
         </form>
