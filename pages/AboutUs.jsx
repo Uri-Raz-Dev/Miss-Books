78dbc1664c807e605262889908149e9135650acd
Uri-Raz-Dev <147313459+Uri-Raz-Dev@users.noreply.github.com>
2024-05-18 22:45:16 +0300

    Add about us and home filler

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -1,4 +1,4 @@
 
 export function AboutUs() {
-    return <h1>about</h1>
+    return <h1>“It is only when we take chances that our lives improve. The initial and the most difficult risk we need to take is to become honest.” —Walter Anderson</h1>
 }
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,4 +1,4 @@
 
 export function HomePage() {
-    return <h1>home</h1>
+    return <h1>When you give joy to other people, you get more joy in return. You should give a good thought to the happiness that you can give out.” —Eleanor Roosevelt</h1>
 }
